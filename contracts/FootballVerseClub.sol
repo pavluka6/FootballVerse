8fff901f81d12a228066a333398bffd096cabdfa
Luka Pavlović <luka.pavlovic@visagetechnologies.com>
2022-08-18 01:19:05 +0200

    Connected two contracts via an interface, minting first batch of players available

--- contracts/FootballVerseClub.sol
+++ contracts/FootballVerseClub.sol
@@ -9,13 +9,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @title FootballVerseClub
  * @dev MVP version of the FootballVerse Club Contract
  */
+
+interface PlayerInterface {
+  function createPlayer(address _to, uint256 clubId) external;
+}
+
 contract FootballVerseClub is ERC721, Ownable {
     uint256 index;
+    address playerContractAddress;
 
     struct Club {
         string name;
         uint8 stadium;
         uint8 academy;
+        bool initialized;
     }
 
     uint256 fee = 1 ether;
@@ -23,12 +30,15 @@ contract FootballVerseClub is ERC721, Ownable {
     Club[] public clubs;
 
     event NewClub(address indexed owner, uint256 index);
+    event FirstPlayersMinted(address indexed owner, uint256 index);
+
+    PlayerInterface playerContract;
 
     constructor(string memory tokenName, string memory tokenSymbol) ERC721(tokenName, tokenSymbol) {}
 
     function _createClub(string memory _name) public payable {
         require(msg.value >= fee, "Value sent is lower than the price");
-        Club memory newClub = Club(_name, 1, 1);
+        Club memory newClub = Club(_name, 1, 1, false);
         clubs.push(newClub);
         _safeMint(msg.sender, index);
         index++;
@@ -47,5 +57,40 @@ contract FootballVerseClub is ERC721, Ownable {
 
     function getClubs() public view returns(Club[] memory) {
         return clubs;
+    }
+
+    function getOwnerClubs(address _owner) public view returns (Club[] memory) {
+        Club[] memory result = new Club[](balanceOf(_owner));
+        uint8 counter = 0;
+
+        for(uint256 i=0; i < clubs.length; i++) {
+            if(ownerOf(i) == _owner) {
+                result[counter] = clubs[i];
+                counter++;
+            }
+        }
+
+        return result;
+    }
+
+    function changeClubName(uint256 indexOf, string memory _name) external {
+        require(ownerOf(indexOf) == msg.sender);
+        clubs[indexOf].name = _name;
+    }
+
+     function setPlayerContractAddress(address _address) external onlyOwner {
+        playerContractAddress = _address;
+        playerContract = PlayerInterface(playerContractAddress);
+    }
+
+    function mintFirstPlayers(uint256 clubId) public {
+        require(msg.sender == ownerOf(clubId));
+        require(!clubs[clubId].initialized);
+        for (uint i = 0; i < 16; i++) { 
+            playerContract.createPlayer(msg.sender, clubId);
+        }
+
+        clubs[clubId].initialized = true;
+        emit FirstPlayersMinted(msg.sender, clubId);
     }
 }
--- contracts/FootballVersePlayer.sol
+++ contracts/FootballVersePlayer.sol
@@ -4,27 +4,56 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+
 
 /**
  * @title FootballVersePlayer
-* @dev MVP version of the FootballVerse Club Contract
+ * @dev Create a sample ERC721 standard token
  */
 contract FootballVersePlayer is ERC721, Ownable {
-    uint256 index = 0;
+    mapping (uint256 => uint256) public playerToClub;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    Player[] public players;
+
+    string[] public playerNames = ['Lionel', 'Cristiano', 'Luka', 'Kylian', 'Erling'];
+    string[] public playerSurnames = ['Messi', 'Ronaldo', 'Modric', 'Mbappe', 'Haaland'];
+ 
+    event PlayerMinted(address indexed _to, uint256 clubId, uint256 playerId);
 
     struct Player {
         string name;
         string surname;
-        uint8 att;
-        uint8 mid;
-        uint8 def;
-        uint8 age;
+        uint256 att;
+        uint256 mid;
+        uint256 def;
+        uint256 age;
     }
 
     constructor(string memory tokenName, string memory tokenSymbol) ERC721(tokenName, tokenSymbol) {}
 
-    function mintPlayer(address _to) public payable {
-        _safeMint(_to, index);
-        index+=1;
+    // modifier onlyOwnerOf(uint clubId) {
+    //     require(msg.sender == ownerOf(clubId));
+    // }
+
+    function createPlayer(address _to, uint256 clubId) external {
+        uint playerId = _tokenIds.current();
+        uint rId_name = uint(keccak256(abi.encodePacked(block.difficulty, msg.sender)));
+        uint rId_surname = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, msg.sender)));
+        bytes memory x = abi.encodePacked(block.timestamp, msg.sender);
+        Player memory newPlayer = Player(playerNames[rId_name%5], playerSurnames[rId_surname%5], uint(keccak256(x)) % 100, uint(keccak256(x)) % 100, uint(keccak256(x)) % 100, uint(keccak256(x))%17 + 18);
+        players.push(newPlayer);
+        playerToClub[playerId] = clubId;
+        _safeMint(_to, playerId);
+        _tokenIds.increment();
+
+        emit PlayerMinted(_to, clubId, playerId);
     }
+
+    function getPlayers() public view returns(Player[] memory) {
+        return players;
+    }
+
 }
